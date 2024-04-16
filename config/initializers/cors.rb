5c9a79d5970a7cf57d4132025ad59fbd2df0c39d
ashahi322@gmail.com <ashahi322@gmail.com>
2024-04-16 16:40:06 +0545

    Modified cors setup to accept request from deployed FE url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "http://localhost:3000", "http://localhost:3001","http://localhost:5173"
+     origins "http://localhost:3000", "http://localhost:3001","http://localhost:5173","https://insurance-management-system-fe.vercel.app/"
 
      resource "*",
        headers: :any,
